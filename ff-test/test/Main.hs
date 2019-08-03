e8ab6106217581596b5757b10372cabe2f252aa9
Yuriy Syrovetskiy <cblp@cblp.su>
2019-08-03 14:14:05 +0300

    Update ron

--- ff-test/test/Main.hs
+++ ff-test/test/Main.hs
@@ -1,20 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 
-module Main (main) where
+module Main
+  ( main
+    )
+where
 
-import           Test.Tasty (defaultMain, testGroup)
-import           System.IO.Temp (withSystemTempDirectory)
-
-import           Config (configTests)
-import           Database (dataTests)
-import           Readme (readmeTest)
-import           Regression (mkRegressionTest)
+import Config (configTests)
+import Database (databaseTests)
+import Readme (readmeTest)
+import Regression (mkRegressionTest)
+import System.IO.Temp (withSystemTempDirectory)
+import Test.Tasty (defaultMain, testGroup)
 
 main :: IO ()
 main = do
-    regressionTest <- mkRegressionTest
-    withSystemTempDirectory "ff-test" $ \tmp ->
-        defaultMain $
-            testGroup ""
-                [configTests, dataTests, readmeTest, regressionTest tmp]
+  regressionTest <- mkRegressionTest
+  withSystemTempDirectory "ff-test" $ \tmp ->
+    defaultMain
+      $ testGroup ""
+          [configTests, databaseTests, readmeTest, regressionTest tmp]
--- ff-test/test/Regression.hs
+++ ff-test/test/Regression.hs
@@ -2,63 +2,71 @@
 
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Regression (mkRegressionTest) where
+module Regression
+  ( mkRegressionTest
+    )
+where
 
-import           Data.Aeson (ToJSON)
-import           Data.Aeson.TH (defaultOptions, deriveToJSON)
-import           Data.Traversable (for)
-import           Data.Yaml (encodeFile)
-import           RON.Data (evalObjectState, getObject)
-import           RON.Data.RGA (RGA)
-import           RON.Storage (Collection, CollectionName, DocId, loadDocument)
-import           RON.Storage.Backend (Document (Document, objectFrame),
-                                      getCollections, getDocuments)
-import           RON.Storage.FS (Handle, newHandle, runStorage)
-import           System.Directory (createDirectoryIfMissing)
-import           System.FilePath (takeDirectory, (</>))
-import           Test.Tasty (TestTree, testGroup)
-import           Test.Tasty.Golden (goldenVsFileDiff)
-
-import           FF.Types (Note, NoteStatus, Status, Track)
+import Data.Aeson.TH (defaultOptions, deriveToJSON)
+import Data.Traversable (for)
+import Data.Yaml (encodeFile)
+import RON.Storage.FS (Handle, newHandle, runStorage)
+import FF.Types
+  ( Entity (Entity, entityVal),
+    Note,
+    NoteId,
+    NoteStatus,
+    Status,
+    Track,
+    loadNote
+    )
+import RON.Data.RGA (RGA)
+import RON.Storage (CollectionName)
+import RON.Storage.Backend (getCollections, getDocuments)
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath ((</>), takeDirectory)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.Golden (goldenVsFileDiff)
 
 deriveToJSON defaultOptions ''RGA
 
 deriveToJSON defaultOptions ''Note
+
 deriveToJSON defaultOptions ''NoteStatus
+
 deriveToJSON defaultOptions ''Status
+
 deriveToJSON defaultOptions ''Track
 
 mkRegressionTest :: IO (FilePath -> TestTree)
 mkRegressionTest = do
-    h <- newHandle "../.ff"
-    collections <- runStorage h getCollections
-    tests <- for collections $ \collection -> case collection of
-        "note" -> testCollection @Note h collection
-        _      -> fail $ "unsupported type " ++ show collection
-    pure $ \tmp -> testGroup "regression" [test tmp | test <- tests]
+  h <- newHandle "../.ff"
+  collections <- runStorage h getCollections
+  tests <-
+    for collections $ \collection -> case collection of
+      "note" -> testNoteCollection h collection
+      _ -> fail $ "unsupported type " ++ show collection
+  pure $ \tmp -> testGroup "regression" [test tmp | test <- tests]
 
-testCollection
-    :: forall a
-    . (Collection a, ToJSON a)
-    => Handle -> CollectionName -> IO (FilePath -> TestTree)
-testCollection h collectionName = do
-    docs <- runStorage h $ getDocuments @_ @a
-    pure $ \tmp -> testGroup collectionName $ map (testDoc h tmp) docs
+testNoteCollection :: Handle -> CollectionName -> IO (FilePath -> TestTree)
+testNoteCollection h collectionName = do
+  docs <- runStorage h $ getDocuments @_ @Note
+  pure $ \tmp -> testGroup collectionName $ map (testNote h tmp) docs
 
-testDoc :: (Collection a, ToJSON a) => Handle -> FilePath -> DocId a -> TestTree
-testDoc h tmp docid =
-    goldenVsFileDiff (show docid) diff ("ff.dump" </> show docid) outFile action
+testNote :: Handle -> FilePath -> NoteId -> TestTree
+testNote h tmp docid =
+  goldenVsFileDiff (show docid) diff ("ff.dump" </> show docid) outFile action
   where
     outFile = tmp </> show docid
     action = do
-        Document{objectFrame} <- runStorage h $ loadDocument docid
-        val <- either (fail . show) pure $ evalObjectState objectFrame getObject
-        createDirectoryIfMissing True $ takeDirectory outFile
-        encodeFile outFile val
+      Entity {entityVal = val} <- runStorage h $ loadNote docid
+      createDirectoryIfMissing True $ takeDirectory outFile
+      encodeFile outFile val
 
 diff :: String -> String -> [String]
 diff ref new = ["colordiff", "-u", ref, new]
